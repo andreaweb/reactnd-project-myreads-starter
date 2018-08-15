c4d9d6e9b78a067be9d50e8cbc3f4e32cf11c88c
andreaweb <deiainluv@gmail.com>
2018-08-15 10:46:10 -0300

    changed this.state.books[key] to simply book

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -30,8 +30,38 @@ export default class BookShelves extends React.Component {
                   <ol className="books-grid">
                   { this.state.books && this.state.books.length > 1
                     ?/* loop through every book in the current bookshelf*/
-                    this.state.books.filter(book => book.shelf === this.state.bookshelvesValues[key]).map(book => (console.log(book)))
-              : null }
+                    this.state.books.filter(book => book.shelf === this.state.bookshelvesValues[key])
+                    .map(
+                      (book, key) => (
+                        <li key={key}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div 
+                                className="book-cover" 
+                                style={{width: 128, height: 188, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : 
+                                  'http://phillyjamz953fm.com/wp-content/plugins/penci-portfolio//images/no-thumbnail.jpg'})` }}
+                              >
+                              </div>
+                              <div className="book-shelf-changer">
+                                <select>
+                                  <option value="move" disabled>Move to...</option>
+                                  <option value="currentlyReading">Currently Reading</option>
+                                  <option value="wantToRead">Want to Read</option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                            <div className="book-title">Rating: {book.averageRating}</div>
+                            <div className="book-authors">Pages: {book.pageCount}</div>
+                            <div className="book-title">{book.description}</div>
+                          </div>
+                        </li> 
+                      )
+                    )
+                    : null }
                 </ol>
                 </div>
               </div> 
