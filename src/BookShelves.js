10a5796386eadbd3ae5c7bc3a83faf59ec578fc5
andreaweb <deiainluv@gmail.com>
2018-08-15 11:50:18 -0300

    authors shown with comma and space between names

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -64,7 +64,7 @@ export default class BookShelves extends React.Component {
                               </div>
                             </div>
                             <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors}</div>
+                            <div className="book-authors">{book.authors.join(', ')}</div>
                             <div className="book-title">Rating: {book.averageRating}</div>
                             <div className="book-authors">Pages: {book.pageCount}</div>
                           </div>
