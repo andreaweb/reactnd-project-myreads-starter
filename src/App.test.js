8e04e4cb6fb685c644d70ef2461eba6dd34d638c
andreaweb <deiainluv@gmail.com>
2018-09-17 17:15:31 -0300

    testing default API call

--- src/App.test.js
+++ src/App.test.js
@@ -1,43 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { Books } from './Books'
-import { Enzyme, configure, shallow} from 'enzyme'
+import { Books } from './components/Books'
+import { Enzyme, configure, shallow, mount } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import renderer from 'react-test-renderer';
+import { XMLHttpRequest } from 'xmlhttprequest';
+global.XMLHttpRequest = XMLHttpRequest;
+import * as BooksAPI from './api/BooksAPI'
 import sinon from 'sinon'
 
 configure({adapter: new Adapter()});
 
-// Wanted to test but couldn't get stuff to work, will try again some other time
+describe('API Call', () => {
+  it('gets books by default', async () => {
+  	    const data = await BooksAPI.getAll()
+  	    console.log("Number of books total: "+data.length)
+    	expect(data).not.toHaveLength(0)
+    // return BooksAPI.getAll()
+    // 	.then(
+    // 		(books) => console.log('Success')
+    // 	)
+    // 	.catch(
+    // 		(err) => console.log('Error '+err)
+    // 	)
+  })
+})
+describe('Bookshelves', () => {
+  it('renders 3 bookshelves', () => {
 
+  })
+})
 
-// test('Book change shelves', () => {
-// 	const component = renderer.create(<Books state={'book': {'title': 'A volta'} }/>);
-// 	let tree = component.toJSON();
-// 	expect(tree).toMatchSnapshot();
-
-// 	console.log('hallos')
-// })
-
-// test('Books are rendered in correct shelf', () => {
-// 	const component = renderer.create(
-// 		<Books 
-// 			index={0} 
-// 			books={[{'title': 'A Origem', 'shelf': 'currentlyReading'}]} 
-// 			bookshelvesValues={["currentlyReading", "wantToRead", "read"]} 
-// 		/>
-// 	);
-// 	let tree = component.toJSON();
-// 	expect(tree).toMatchSnapshot();
-
-// 	console.log(tree)
-// })
-// const spy = sinon.spy(Books.prototype, 'componentWillReceiveProps');
-
-// const wrapper = shallow(<Books foo="bar" />);
-// console.log()
-// expect(spy).to.have.property('callCount', 0);
-// wrapper.setProps({ foo: 'foo' });
-// expect(spy).to.have.property('callCount', 1);
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,31 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-var chai = require('chai'); 
 import chaiEnzyme from 'chai-enzyme'
- 
-chai.use(chaiEnzyme())
-
-const originalNot = Object.getOwnPropertyDescriptor(chai.Assertion.prototype, 'not').get;
-Object.defineProperty(chai.Assertion.prototype, 'not', {
-  get() {
-    Object.assign(this, this.assignedNot);
-    return originalNot.apply(this);
-  },
-  set(newNot) {
-    this.assignedNot = newNot;
-    return newNot;
-  },
-});
 
 // Combine both jest and chai matchers on expect
 const originalExpect = global.expect;
-
-global.expect = (actual) => {
-  const originalMatchers = originalExpect(actual);
-  const chaiMatchers = chai.expect(actual);
-  const combinedMatchers = Object.assign(chaiMatchers, originalMatchers);
-  return combinedMatchers;
-};
 
 configure({ adapter: new Adapter() });
 const localStorageMock = {
